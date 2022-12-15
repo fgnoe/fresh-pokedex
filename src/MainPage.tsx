0298c9305027b2ae61f374270686307f20bb6687
Noe <noe.flores@relometrics.com>
2022-12-15 16:44:17 -0700

    add cycling themes

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,9 +1,31 @@
 import PokemonCard from './components/pokemon/PokemonCard'
 import './MainPage.css'
+import {useEffect, useState} from "react";
+
+const themes = [
+    'light', 'dark', 'cupcake',
+    'bumblebee', 'emerald', 'corporate',
+    'synthwave', 'retro', 'cyberpunk',
+    'valentine', 'halloween', 'garden',
+    'forest', 'aqua', 'lofi',
+    'pastel', 'fantasy', 'wireframe',
+    'black', 'luxury', 'dracula',
+    'cmyk', 'autumn', 'business',
+    'acid', 'lemonade', 'night',
+    'coffee', 'winter',
+]
 
 function MainPage() {
+    const [themeIdx, setThemeIdx] = useState(0);
+    useEffect(() => {
+        const id = setInterval(() =>
+            setThemeIdx((oldCount) => (oldCount + 1) % themes.length), 1000);
+        return () => {
+            clearInterval(id);
+        };
+    }, []);
     return (
-        <div className="App">
+        <div className="App" data-theme={themes[themeIdx]}>
             <header className="App-header">
                 <PokemonCard/>
             </header>
