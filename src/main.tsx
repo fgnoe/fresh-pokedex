0d155156fedc183e69e2debcf8413bb247151872
Noe <noe.flores@relometrics.com>
2022-12-15 16:11:05 -0700

    add error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import MainPage from './MainPage'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, RouterProvider, useRouteError} from "react-router-dom";
 import {ThemeProvider} from "next-themes";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <MainPage/>,
+        errorElement: <ErrorBoundary/>,
     },
 ], {basename: "/fresh-pokedex"});
+
+function ErrorBoundary() {
+    let error = useRouteError();
+    console.error(error);
+    // Uncaught ReferenceError: path is not defined
+    return <div>Dang!</div>;
+}
 
 ReactDOM.render(
     <React.StrictMode>
