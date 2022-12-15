9b35654c141c7a1833fe7af8738b8f60ba65bd1a
Noe <noe.flores@relometrics.com>
2022-12-15 12:07:01 -0700

    proper tailwind usage

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
 import PokemonCard from './components/pokemon/PokemonCard'
 import './MainPage.css'
 
 function MainPage() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <PokemonCard />
-      </header>
-    </div>
-  )
+    return (
+        <div className="App">
+            <header className="App-header">
+                <PokemonCard/>
+            </header>
+        </div>
+    )
 }
 
 export default MainPage
--- src/components/pokemon/PokemonCard.tsx
+++ src/components/pokemon/PokemonCard.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 const PokemonCard = () => {
     return (
 
-        <div className="card w-96 bg-base-100 shadow-xl">
-            <figure className="px-10 pt-10">
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png" className="rounded-xl" />
+        <div className="card w-60 bg-base-100 shadow-xl">
+            <figure className="px-10 mt-10">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png" className={'-mb-5'} />
             </figure>
             <div className="card-body items-center text-center">
                 <h2 className="card-title">Squirtle</h2>
