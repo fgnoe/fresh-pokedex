c1975614ec71a355275fc035fc42a5bc8e9e4286
Noe <noe.flores@relometrics.com>
2022-12-15 10:19:32 -0700

    cleanup and use daisy ui

--- src/components/pokemon/PokemonCard.tsx
+++ src/components/pokemon/PokemonCard.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import {Card, Col, Text} from "@nextui-org/react";
 
 const PokemonCard = () => {
     return (
-        <Card css={{ w: "130px", h: "190px" }}>
-            <Card.Header css={{ position: "absolute", zIndex: 1, top: 12, }}>
-                <Col>
-                    <Text h4 color="white">
-                        Squirtle
-                    </Text>
-                </Col>
-            </Card.Header>
-            <Card.Image
-                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png"
-                width="100%"
-                css={{marginTop: 14}}
-                height={150}
-                objectFit="contain"
-            />
-        </Card>
+
+        <div className="card w-96 bg-base-100 shadow-xl">
+            <figure className="px-10 pt-10">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png" className="rounded-xl" />
+            </figure>
+            <div className="card-body items-center text-center">
+                <h2 className="card-title">Squirtle</h2>
+            </div>
+        </div>
     );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import MainPage from './MainPage'
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import {createTheme, NextUIProvider} from '@nextui-org/react';
 import {ThemeProvider} from "next-themes";
-
-const lightTheme = createTheme({
-    type: 'light',
-    theme: {}
-})
-
-const darkTheme = createTheme({
-    type: 'dark',
-    theme: {}
-})
 
 const router = createBrowserRouter([
     {
@@ -29,13 +18,11 @@ ReactDOM.render(
             defaultTheme="system"
             attribute="class"
             value={{
-                light: lightTheme.className,
-                dark: darkTheme.className
+                light: "light",
+                dark: "dark",
             }}
         >
-            <NextUIProvider>
                 <RouterProvider router={router}/>
-            </NextUIProvider>
         </ThemeProvider>
     </React.StrictMode>,
     document.getElementById('root')
