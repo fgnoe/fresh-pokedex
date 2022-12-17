4cca2fd674e47a03e0761621b68df6cc3fef9c16
Noe <noe.flores@relometrics.com>
2022-12-16 19:43:48 -0700

    add list for first 151 pokemon

--- src/components/pokemon-list/PokemonList.tsx
+++ src/components/pokemon-list/PokemonList.tsx
@@ -1,9 +1,16 @@
 import PokemonCard from "../pokemon/PokemonCard";
 import './PokemonList.css'
+import pokeapi from "../utils/pokeapi";
+import {useEffect, useState} from "react";
 
 const PokemonList = () => {
+    const cards = [...Array(151).keys()].map(x => ++x)
+        .map(n => {
+        return <PokemonCard pokemonId={n}/>;
+    });
+
     return <div className="pokemon-list">
-        <PokemonCard/>
+        {cards}
     </div>
 }
 
--- src/components/pokemon/PokemonCard.tsx
+++ src/components/pokemon/PokemonCard.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Pokemon} from "../../types";
+import pokeapi from "../utils/pokeapi";
 
-const PokemonCard = () => {
+type PokemonCardProps = {
+    pokemonId: number;
+};
+
+const PokemonCard = ({pokemonId}: PokemonCardProps) => {
+    const [pokemon, setPokemon] = useState<any>(null);
+    useEffect(() => {
+        pokeapi.get(`/pokemon/${pokemonId}`)
+            .then(response => {
+                setPokemon(response.data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }, []);
+
     return (
-        <div className="card w-60 bg-base-100 shadow-xl">
+        <div className="card w-60 bg-base-100 shadow-xl ml-2 mr-2 mt-2">
             <figure className="px-10 mt-10">
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png" className={'-mb-5'} />
+                <img src={pokemon?.sprites?.front_default} className={'-mb-5'} />
             </figure>
             <div className="card-body items-center text-center">
-                <h2 className="card-title">Squirtle</h2>
+                <h2 className="card-title">{pokemon?.species?.name}</h2>
             </div>
         </div>
     );
